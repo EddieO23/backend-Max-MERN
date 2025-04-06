ae4e67ff2cad65533ffc6e2ee2078fc46f2ced90
Eddie <eocampo52@gmail.com>
2025-04-05 21:08:04 -0500

    added delete function. this function deletes a placeID.

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -2,7 +2,7 @@ const { v4: uuidv4 } = require('uuid');
 
 const HttpError = require('../models/http-error');
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: 'p1',
     title: 'Chicago',
@@ -92,7 +92,9 @@ const updatePlace = (req, res, next) => {
 }
 
 const deletePlace = (req, res, next) => {
-
+  const placeId = req.params.pid
+  DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId)
+  res.status(200).json({msg: 'Succesfully deleted a place.'})
 }
 
 exports.getPlaceById = getPlaceById;
